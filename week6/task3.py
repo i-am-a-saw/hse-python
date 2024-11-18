8e68565cd4de63bdbf886a688373d53c7507f27d
Alexander Fedoriako <139990833+i-am-a-saw@users.noreply.github.com>
2024-11-18 11:40:40 +0300

    Create task3.py

---
+++ week6/task3.py
@@ -0,0 +1,20 @@
+from collections import defaultdict
+
+
+class Solution:
+    def max_subarray_length(self, nums: List[int], k: int) -> int:
+        frequency_counter = defaultdict(int)
+        max_length = 0
+        left_pointer = 0
+
+        for right_pointer, value in enumerate(nums):
+            frequency_counter[value] += 1
+
+            while frequency_counter[value] > k:
+                frequency_counter[nums[left_pointer]] -= 1
+                left_pointer += 1
+
+            current_length = right_pointer - left_pointer + 1
+            max_length = max(max_length, current_length)
+
+        return max_length
